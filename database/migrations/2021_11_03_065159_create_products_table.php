cb723b0fa3051bf96ca90631bda2d67e3ba27f05
rzhasibuan <rzhasibuan@gmail.com>
2021-10-18 14:20:23 +0700

    migrate

--- database/migrations/2021_11_03_065159_create_products_table.php
+++ database/migrations/2021_11_03_065159_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->double('price');
+            $table->text('description');
             $table->timestamps();
         });
     }
