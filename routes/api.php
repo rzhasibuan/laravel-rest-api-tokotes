9200e7020186f41e6de78a962b7842c5650a19ca
rzhasibuan <rzhasibuan@gmail.com>
2021-10-22 23:54:27 +0700

    fetch category and add route

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 //Route::get('your-lastname', fn () => response()->json(['name' => 'hasibuan']));
 
 Route::apiResource('products', \App\Http\Controllers\ProductController::class);
+Route::apiResource('category', \App\Http\Controllers\CategoryController::class);
+
